b0fa68562c4adf3eaceb17066bbce635da976fb7
Steven Velez <stevendvelez@yahoo.com>
2020-06-14 10:09:36 -0400

    updated cloud im on CTAButton

--- src/components/CTAButton/CTAButton.jsx
+++ src/components/CTAButton/CTAButton.jsx
@@ -5,8 +5,7 @@ import classes from "./CTAButton.module.css";
 const CTAButton = (props) => {
     return (
         <div onClick={() => props.onClick("redirect")} className={classes.CTAButton}>
-            <img src="https://firebasestorage.googleapis.com/v0/b/wickid-sense.appspot.com/o/images%2FwickidCloud.svg?alt=media&token=aedc2c82-4719-4c4b-beba-77629b02e62a" alt="wickid-cloud"/>
-            <p>shop our wares</p>
+            <img src="https://firebasestorage.googleapis.com/v0/b/wickid-sense.appspot.com/o/images%2FwickidCloud.svg?alt=media&token=e1db8abe-8caf-4689-89a2-a01406a30951" alt="wickid-cloud"/>
         </div>
     )
 }
--- src/components/UI/GlobalLoader/GlobalLoader.jsx
+++ src/components/UI/GlobalLoader/GlobalLoader.jsx
@@ -5,7 +5,7 @@ import classes from './GlobalLoader.module.css';
 const GlobalLoader = () => {
     return (
         <div className={classes.GlobalLoader}>
-            <div className={classes.Loader}>Loading...</div>
+            <img src="https://firebasestorage.googleapis.com/v0/b/wickid-sense.appspot.com/o/images%2FwickidCloudLoader.svg?alt=media&token=0a6c3211-df0a-49c6-98fa-abed9380d3ee" alt="wickid-cloud-loading"/>
         </div>
     );
 };
