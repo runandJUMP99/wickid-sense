14d3ceb2a600799378ef5ee3e36639a302baa27b
Steven Velez <stevendvelez@yahoo.com>
2020-06-16 07:25:40 -0400

    update wickidCloud svg

--- src/components/CTAButton/CTAButton.jsx
+++ src/components/CTAButton/CTAButton.jsx
@@ -5,7 +5,7 @@ import classes from "./CTAButton.module.css";
 const CTAButton = (props) => {
     return (
         <div onClick={() => props.onClick("redirect")} className={classes.CTAButton}>
-            <img src="https://firebasestorage.googleapis.com/v0/b/wickid-sense.appspot.com/o/images%2FwickidCloud.svg?alt=media&token=e1db8abe-8caf-4689-89a2-a01406a30951" alt="wickid-cloud"/>
+            <img src="https://firebasestorage.googleapis.com/v0/b/wickid-sense.appspot.com/o/images%2FwickidCloud.svg?alt=media&token=6d263bdd-9a22-46c6-acd3-d59c26d0cf2f" alt="wickid-cloud"/>
         </div>
     )
 }
